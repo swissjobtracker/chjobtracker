a4e8d8cf63730f60e6ba2dbe1b8d46a6e5c4194f
Stéphane Bisinger <bisinger@kof.ethz.ch>
2023-04-03 10:42:26 +0200

    Add grants for the new table

--- inst/db/migrations/000002_add_indicator_history.up.sql
+++ inst/db/migrations/000002_add_indicator_history.up.sql
@@ -2,3 +2,6 @@ CREATE TABLE IF NOT EXISTS "indicator_history" (
   date DATE PRIMARY KEY,
   values json NOT NULL
   );
+
+GRANT SELECT ON x28.indicator_history TO x28_reader;
+GRANT SELECT, INSERT, UPDATE, DELETE ON x28.indicator_history TO x28_reader;
