d279c973ed63890629e3e6975ba807d0d3cf95d6
Stéphane Bisinger <bisinger@kof.ethz.ch>
2023-11-10 14:06:21 +0100

    Add empty company.crn if missing

--- R/parse_x28_chunk.R
+++ R/parse_x28_chunk.R
@@ -11,10 +11,9 @@
 #' @importFrom urltools domain
 #' @export
 parse_x28_chunk <- function(chunk = data.frame(), text = NULL) {
-
-  if(!is.null(text)) {
+  if (!is.null(text)) {
     # From dump
-    chunk_parsed <- as.data.table(fromJSON(paste0('[', paste(text, collapse = ","), ']')))
+    chunk_parsed <- as.data.table(fromJSON(paste0("[", paste(text, collapse = ","), "]")))
   } else {
     # From api with as = "parsed", simplifyVector = TRUE
     chunk_parsed <- as.data.table(chunk)
@@ -33,63 +32,65 @@ parse_x28_chunk <- function(chunk = data.frame(), text = NULL) {
   chunk_names <- names(chunk_parsed)
 
   # introduced starting 2019
-  if(!"origin" %in% chunk_names) {
+  if (!"origin" %in% chunk_names) {
     message("inserting origin column")
     chunk_parsed[, origin := NA]
   }
 
   # switches beginning 2019
-  if("quotamax" %in% chunk_names && "workquota.maximum" %in% chunk_names) {
+  if ("quotamax" %in% chunk_names && "workquota.maximum" %in% chunk_names) {
     chunk_parsed[is.na(workquota.maximum), workquota.maximum := quotamax]
     chunk_parsed[is.na(workquota.minimum), workquota.minimum := quotamin]
-  } else if("quotamax" %in% chunk_names) {
+  } else if ("quotamax" %in% chunk_names) {
     chunk_parsed[, workquota.maximum := quotamax]
     chunk_parsed[, workquota.minimum := quotamin]
   }
 
   # 2019
-  if("firstseen" %in% chunk_names && "created" %in% chunk_names) {
+  if ("firstseen" %in% chunk_names && "created" %in% chunk_names) {
     chunk_parsed[is.na(created), created := firstseen]
     chunk_parsed[is.na(updated), updated := lastseen]
-  } else if("firstseen" %in% chunk_names) {
+  } else if ("firstseen" %in% chunk_names) {
     chunk_parsed[, created := firstseen]
     chunk_parsed[, updated := lastseen]
   }
 
-  if(!"company.uid" %in% chunk_names) {
+  if (!"company.uid" %in% chunk_names) {
     chunk_parsed[, company.uid := NA]
+  }
+  if (!"company.crn" %in% chunk_names) {
     chunk_parsed[, company.crn := NA]
   }
 
   # Either pre nested data (as above) or the occasional page from the api
   # lack company size information
-  if(!any(grepl("company.size", chunk_names))) {
+  if (!any(grepl("company.size", chunk_names))) {
     chunk_parsed[, company.size.id := NA]
     chunk_parsed[, company.size.name := NA]
     chunk_parsed[, company.size.employees.minimum := NA]
     chunk_parsed[, company.size.employees.maximum := NA]
   }
 
-  if(mode(chunk_parsed$created) == "character") {
+  if (mode(chunk_parsed$created) == "character") {
     chunk_parsed[, created_parsed := parse_ISO8601(created)]
     chunk_parsed[, updated_parsed := parse_ISO8601(updated)]
 
     # on the API, deleted is a boolean flag indicating if an ad is marked
     # as deleted. in the dumps it is a timestamp
-    if(mode(chunk_parsed$deleted) == "logical") {
+    if (mode(chunk_parsed$deleted) == "logical") {
       chunk_parsed[deleted == TRUE, deleted_parsed := updated_parsed[deleted]]
     } else {
       # this case should probably never occur (neither the is.logical one below)
       chunk_parsed[, deleted_parsed := parse_ISO8601(deleted)]
     }
   } else {
-    chunk_parsed[, created_parsed := as.POSIXct(created/1000, origin = "1970-01-01")]
-    chunk_parsed[, updated_parsed := as.POSIXct(updated/1000, origin = "1970-01-01")]
+    chunk_parsed[, created_parsed := as.POSIXct(created / 1000, origin = "1970-01-01")]
+    chunk_parsed[, updated_parsed := as.POSIXct(updated / 1000, origin = "1970-01-01")]
 
-    if(mode(chunk_parsed$deleted) == "logical") {
+    if (mode(chunk_parsed$deleted) == "logical") {
       chunk_parsed[deleted == TRU, deleted_parsed := updated_parsed[deleted]]
     } else {
-      chunk_parsed[, deleted_parsed := as.POSIXct(deleted/1000, origin = "1970-01-01")]
+      chunk_parsed[, deleted_parsed := as.POSIXct(deleted / 1000, origin = "1970-01-01")]
     }
   }
 
@@ -104,11 +105,11 @@ parse_x28_chunk <- function(chunk = data.frame(), text = NULL) {
     updated = updated_parsed,
     deleted = deleted_parsed,
     language,
-    qualityscore = if("qualityscore" %in% chunk_names) qualityscore else NA, # TODO: this is not in the dumps?
+    qualityscore = if ("qualityscore" %in% chunk_names) qualityscore else NA, # TODO: this is not in the dumps?
     workquota_minimum = workquota.minimum,
     workquota_maximum = workquota.maximum,
     temporary,
-    homeoffice = if("homeoffice" %in% chunk_names) homeoffice else NA,
+    homeoffice = if ("homeoffice" %in% chunk_names) homeoffice else NA,
     url,
     domain = get_root_domain(url),
     company_url = company.url,
@@ -129,12 +130,12 @@ parse_x28_chunk <- function(chunk = data.frame(), text = NULL) {
   ads_details <- chunk_parsed[, .(
     advertisement_id = id,
     title,
-    raw_text = if("content" %in% chunk_names) content else NA_character_
+    raw_text = if ("content" %in% chunk_names) content else NA_character_
   )]
 
   # Positions to occupy
   ads_positions <- chunk_parsed[, .(
-    position = if(is.null(ncol(positions[[1]]))) {
+    position = if (is.null(ncol(positions[[1]]))) {
       unlist(positions)
     } else {
       as.character(positions[[1]]$value)
@@ -143,8 +144,8 @@ parse_x28_chunk <- function(chunk = data.frame(), text = NULL) {
 
   # Education being asked for
   ads_educations <- chunk_parsed[, .(
-    education = if(is.null(ncol(educations[[1]]))) {
-      if(length(educations[[1]]) > 0) {
+    education = if (is.null(ncol(educations[[1]]))) {
+      if (length(educations[[1]]) > 0) {
         unlist(educations[[1]])
       } else {
         character()
@@ -157,11 +158,11 @@ parse_x28_chunk <- function(chunk = data.frame(), text = NULL) {
   # Bind all metadata together
   # Some entries appear to be duplicated (e.g. if skill "informatik" is detected
   # multiple times in content). We only need one of each.
-  ads_metadata <- chunk_parsed[, rbindlist(metadata, idcol = TRUE, fill = TRUE, use.names=TRUE)]
+  ads_metadata <- chunk_parsed[, rbindlist(metadata, idcol = TRUE, fill = TRUE, use.names = TRUE)]
 
   # Pre-2019 there was a separate id that was not the id of the datum which was stored in value
   # 2019 and onward the value is in id so we follow this format
-  if("value" %in% names(ads_metadata)) {
+  if ("value" %in% names(ads_metadata)) {
     ads_metadata[, id := value][, value := NULL]
   }
 
@@ -180,13 +181,15 @@ parse_x28_chunk <- function(chunk = data.frame(), text = NULL) {
 
   ads_metadata <- unique(ads_metadata)
 
-  ads_locations <- chunk_parsed[, rbindlist(locations,
-                                            idcol = TRUE,
-                                            use.names = TRUE,
-                                            fill = TRUE)][, id := chunk_parsed[.id, id]][, .id := NULL]
+  ads_locations <- chunk_parsed[, rbindlist(
+    locations,
+    idcol = TRUE,
+    use.names = TRUE,
+    fill = TRUE
+  )][, id := chunk_parsed[.id, id]][, .id := NULL]
   names(ads_locations) <- tolower(names(ads_locations))
 
-  if(ncol(ads_locations) == 3) {
+  if (ncol(ads_locations) == 3) {
     # Old "type - value" pair type
     ads_country <- ads_locations[type == "COUNTRY", .(advertisement_id = id, country = value)]
     ads_canton <- ads_locations[type == "PROVINCE", .(advertisement_id = id, canton = value)]
@@ -196,19 +199,23 @@ parse_x28_chunk <- function(chunk = data.frame(), text = NULL) {
     ads_canton <- ads_locations[, .(canton = unique(province)), by = .(advertisement_id = id)][!is.na(canton)]
 
     # newer / API format bundles them
-    if("postalcodes" %in% names(ads_locations)) {
+    if ("postalcodes" %in% names(ads_locations)) {
       # Ensure (at least the first) postalcodes is typed
       # otherwise data.table can't infer the type of the new column
       ads_locations[sapply(postalcodes, length) == 0, postalcodes := list(numeric())]
       ads_postalcode <- ads_locations[,
-                                      .(postalcode = as.numeric(unique(unlist(postalcodes)))),
-                                      by = .(advertisement_id = id)][
-                                            !is.na(postalcode)]
+        .(postalcode = as.numeric(unique(unlist(postalcodes)))),
+        by = .(advertisement_id = id)
+      ][
+        !is.na(postalcode)
+      ]
     } else {
       ads_postalcode <- ads_locations[,
-                                      .(postalcode = unique(postalcode)),
-                                      by = .(advertisement_id = id)][
-                                            !is.na(postalcode)]
+        .(postalcode = unique(postalcode)),
+        by = .(advertisement_id = id)
+      ][
+        !is.na(postalcode)
+      ]
     }
   }
   ads_postalcode <- ads_postalcode[postalcode != "null"]
@@ -216,26 +223,30 @@ parse_x28_chunk <- function(chunk = data.frame(), text = NULL) {
   comps_metadata <- rbindlist(chunk_parsed$company.metadata, idcol = "md_id", use.names = TRUE)
 
   # cf. ads_metadata a few lines up there ^^^
-  if("value" %in% names(comps_metadata)) {
+  if ("value" %in% names(comps_metadata)) {
     comps_metadata[, id := value][, value := NULL]
   }
-  comps_metadata <- comps_metadata[, advertisement_id := chunk_parsed[md_id, id]][,
-                                                               company_id := chunk_parsed[md_id, company.id]][,
-                                                                                                              md_id := NULL][, .(
-                                                                                                                advertisement_id,
-                                                                                                                company_id,
-                                                                                                                metadata_id = as.numeric(id),
-                                                                                                                type,
-                                                                                                                name
-                                                                                                              )]
+  comps_metadata <- comps_metadata[, advertisement_id := chunk_parsed[md_id, id]][
+    ,
+    company_id := chunk_parsed[md_id, company.id]
+  ][
+    ,
+    md_id := NULL
+  ][, .(
+    advertisement_id,
+    company_id,
+    metadata_id = as.numeric(id),
+    type,
+    name
+  )]
 
   # the earlier dumps don't contain company addresses
   comps_addresses <- data.table()
-  if("company.addresses" %in% chunk_names) {
+  if ("company.addresses" %in% chunk_names) {
     # The newer dump (~ Jun 2021) has an additional street for (very few) company.addresses
     # get rid of that
     addrs <- lapply(chunk_parsed$company.addresses, function(x) {
-      if(ncol(x) > 0) {
+      if (ncol(x) > 0) {
         x$street <- NULL
       }
       x
